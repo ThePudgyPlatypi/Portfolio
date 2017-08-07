94950fbbb1f526a7bf15881fc1ad7a3f48b8b6d0
ThePudgyPlatypi <stehmc1@tcnj.edu>
2017-08-07 17:19:09 -0400

    Live

--- js/app.js
+++ js/app.js
@@ -3,15 +3,19 @@ $(document).foundation();
 $(document).ready(function() {
     //    variables to grab elements and set the offset of how far I want the elements to sit from the 
     //    main navigation "cube"
-    var cube = document.getElementById("cube");
-    var heading = document.getElementById("heading").getElementsByClassName("slideElement");
-    var nav = document.getElementById("nav").getElementsByClassName("slideElement");
-    var initialOffset = 175;
-    var initialOffsetNav = 100;
+    if (document.getElementById("cube")) {
+        var cube = document.getElementById("cube");
+        var heading = document.getElementById("heading").getElementsByClassName("slideElement");
+        var nav = document.getElementById("nav").getElementsByClassName("slideElement");
+        var initialOffset = 175;
+        var initialOffsetNav = 100;
+        
+        navReveal(heading, nav, initialOffset, initialOffsetNav);
+        spinningHeader();
+        anchorClick();
+    };
     
-    navReveal(heading, nav, initialOffset, initialOffsetNav);
-    spinningHeader();
-    anchorClick();
+    
 });
 
     
